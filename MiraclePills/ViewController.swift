a0820c3112944c16b5822c24d970547553c01ff9
Andrey Piterov <pfm@live.ru>
2017-07-06 21:48:08 +0300

    add UIDataPickerView for states

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alabama", "New York", "California", "Colorado"]
+    
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        self.statePicker.isHidden = false
+        statePicker.dataSource = self
+        statePicker.delegate = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,6 +28,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: .normal)
+        statePicker.isHidden = true
     }
 
 
